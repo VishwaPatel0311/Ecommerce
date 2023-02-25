709607ac681992c1a6ce4d7ccbd2bef5f9c7dbc1
VishwaPatel0311 <86764526+VishwaPatel0311@users.noreply.github.com>
2023-02-25 13:40:52 +0530

    Add files via upload

---
+++ routes/admin/middlewares.js
@@ -0,0 +1,27 @@
+const { validationResult } = require('express-validator');
+
+module.exports = {
+    handleErrors(templateFunc, dataCb) {
+        return async ( req, res, next ) => {
+            const errors = validationResult(req);
+
+            if (!errors.isEmpty()) {
+                let data = {};
+                if (dataCb) {
+                    data = await dataCb(req);
+                }
+
+                return res.send(templateFunc({ errors, ...data }));
+            }
+
+            next(); //call the next middleware function
+        };
+    },
+    requireAuth(req, res, next) {
+        if (!req.session.userId) {
+            return res.redirect('/signin');
+        }
+        
+        next();
+    }
+};
